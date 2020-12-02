bb7a15f72e75284333a5c7e0d85d7a0381ca5e60
LiuDonghao <lss060918@163.com>
2020-12-02 21:11:40 +0800

    V3_登陆Controller_Ldh

--- src/main/java/com/baizhi/LdhAndMSApplication.java
+++ src/main/java/com/baizhi/LdhAndMSApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class LdhAndMSApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(LdhAndMSApplication.class, args);
     }
